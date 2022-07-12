899c151f666bd3583eeb95dbc32697cc57116c64
c3c1l1a <cesswangumukima@gmail.com>
2022-07-12 11:41:42 +0800

    Add styles for completed item

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,11 +1,19 @@
 /* eslint-disable */
 
 function TodoItem(props) {
+  const completedStyle = {
+    fontStyle: 'italic',
+    textDecoration: 'line-through',
+    color: '#595959',
+    opacity: '0.5',
+  }
   return (
     <li>
       <input type="checkbox" checked={props.todo.completed} 
                              onChange={() => props.handleChange(props.todo.id)}/> 
-      {props.todo.title}
+      <span style={props.todo.completed? completedStyle: null}>
+        {props.todo.title}
+      </span>
       <button onClick={() => props.deleteTodoProps(props.todo.id)}>
         Delete
       </button>

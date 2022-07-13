0f5da7ed4aae689b2194ba586d71cc7a83ec5400
c3c1l1a <cesswangumukima@gmail.com>
2022-07-13 14:07:19 +0800

    Push all changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 /* eslint-disable */
+
+import { BrowserRouter as Router } from "react-router-dom";
 import TodoContainer from './components/TodoContainer';
 import "./App.css";
 
 const App = () => {
+
   return (
-    <TodoContainer />
+    <Router>
+      <TodoContainer />
+    </Router>
   );
 }
 
